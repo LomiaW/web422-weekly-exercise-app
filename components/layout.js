339bdddc0f3ce01d45dc026799f4957e035c1250
LomiaW <rwu30@myseneca.ca>
2024-02-01 19:02:48 -0500

    first commit

---
+++ components/layout.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+export default function Layout(props) {
+
+	const router = useRouter();
+
+  return (
+    <>
+      <h1>Pages / Routing in Next.js</h1>
+      <Link href="/">Home</Link> | <Link href="/about">About</Link> | <Link href="/dashboard">Dashboard</Link> | <Link href="/dashboard/preferences">Dashboard Preferences</Link>
+      <hr />
+      <br />
+      {props.children}
+      <br />
+			<hr />
+			<button onClick={() => router.push("/")}>Back Home</button>
+    </>
+  );
+}
